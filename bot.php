210b9fc091be3fd2ff5b3e600579d24ce0e724d2
charttthai <mr.chartthai@gmail.com>
2016-11-19 14:42:22 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,8 +18,14 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'text',
-				'text' => "สวัสดีครับ มีอะไรให้ช่วยไหมคับ";
+				{
+            "type":"text",
+            "text":"สวัสดีครับ มีอะไรให้ช่วยไหมคับ"
+        },
+        {
+            "type":"text",
+            "text":"สอบถามได้คับ"
+        }
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
